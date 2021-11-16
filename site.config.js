381d3076f34c45c9b19116dc3374bf60c5331c59
ORUERT <31624511+ORUERT@users.noreply.github.com>
2021-11-16 20:23:24 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e721439b67f84fde8e9a291fca4d6796',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -8,13 +8,13 @@ module.exports = {
 
   // basic site info (required)
   name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  domain: 'https://flat-hovercraft-661.notion.site/e721439b67f84fde8e9a291fca4d6796',
+  author: 'Delicious Raccoon',
 
   // open graph metadata (optional)
   description: 'Example site description',
   socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  socialImageSubtitle: 'Hello World!',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
