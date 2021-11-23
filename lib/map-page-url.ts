3e01ba716d33e427c713720993bfca076446daa0
ORUERT <31624511+ORUERT@users.noreply.github.com>
2021-11-24 01:15:31 +0800

    Update map-page-url.ts

--- lib/map-page-url.ts
+++ lib/map-page-url.ts
@@ -1,14 +1,11 @@
 import { ExtendedRecordMap } from 'notion-types'
 import { uuidToId, parsePageId } from 'notion-utils'
-
 import { Site } from './types'
 import { includeNotionIdInUrls } from './config'
 import { getCanonicalPageId } from './get-canonical-page-id'
-
 // include UUIDs in page URLs during local development but not in production
 // (they're nice for debugging and speed up local dev)
 const uuid = !!includeNotionIdInUrls
-
 export const mapPageUrl =
   (site: Site, recordMap: ExtendedRecordMap, searchParams: URLSearchParams) =>
   (pageId = '') => {
@@ -21,13 +18,10 @@ export const mapPageUrl =
       )
     }
   }
-
 export const getCanonicalPageUrl =
   (site: Site, recordMap: ExtendedRecordMap) =>
   (pageId = '') => {
-    const index = 0;
-    switch
-
+    const pageUuid = parsePageId(pageId, { uuid: true })
     if (uuidToId(pageId) === site.rootNotionPageId) {
       return `https://${site.domain}`
     } else {
@@ -49,8 +43,6 @@ export const getCannoicalPageSurffix =
       })}`
     }
   }
-
-
 
 function createUrl(path: string, searchParams: URLSearchParams) {
   return [path, searchParams.toString()].filter(Boolean).join('?')
